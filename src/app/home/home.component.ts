675518046313c8e583d2816ce8b02c1474ee44a5
Stephan Raab <sarcartist@gmail.com>
2025-01-15 18:00:02 -0500

    update app with home component input/search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form action="submit">
+        <input type="text" placeholder="filter by city" />
+        <input class="primary" type="submit" value="search" />
+      </form>
+    </section>
+    <section class="results"></section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
