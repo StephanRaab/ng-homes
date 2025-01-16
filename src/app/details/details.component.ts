6145875c2327b9359249a96fe8cf61b9cd16604f
Stephan Raab <sarcartist@gmail.com>
2025-01-15 21:41:16 -0500

    adding a service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,46 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { HousingLocation } from "../housing-location";
 
 @Component({
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` {{ housingLocationId }} `,
+  template: `
+    <article>
+      <img [src]="housingLocation?.photo" alt="" class="listing-photo" />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About location:</h2>
+        <ul>
+          <li>Units Available: {{ housingLocation?.availableUnits }}</li>
+          <li>Includes wifi: {{ housingLocation?.wifi }}</li>
+          <li>Includes Laundry: {{ housingLocation?.laundry }}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply to live here</h2>
+        <button class="primary" type="button">Apply now</button>
+      </section>
+    </article>
+  `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId: number = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params["id"]);
+    const housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
 }
