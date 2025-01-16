fafb79a29e4ea55e7dd79d568c51424832846fc6
Stephan Raab <sarcartist@gmail.com>
2025-01-15 19:49:16 -0500

    add housing location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form action="submit">
@@ -12,7 +13,9 @@ import { CommonModule } from "@angular/common";
         <input class="primary" type="submit" value="search" />
       </form>
     </section>
-    <section class="results"></section>
+    <section class="results">
+      <app-housing-location />
+    </section>
   `,
   styleUrls: ["./home.component.css"],
 })
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-housing-location',
+  selector: "app-housing-location",
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrls: ['./housing-location.component.css']
+  template: ``,
+  styleUrls: ["./housing-location.component.css"],
 })
-export class HousingLocationComponent {
-
-}
+export class HousingLocationComponent {}
